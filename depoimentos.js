ed9a58706a6924f6ad15f792f3c29e76ef2b9330
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-07-11 13:48:32 -0300

    Adicionei depoimentos via JSON

---
+++ depoimentos.js
@@ -0,0 +1,29 @@
+let depoimentos = []
+
+async function buscarDepoimentos() {
+    let response = await fetch('cards.json')
+    depoimentos = await response.json()
+    exibirDepoimentos(depoimentos)
+}
+
+document.addEventListener('DOMContentLoaded', buscarDepoimentos)
+
+function exibirDepoimentos(depoimentos) {
+    let containerDepoimentos = document.querySelector('.container-depoimentos')
+    depoimentos.forEach(depoimento => {
+        containerDepoimentos.innerHTML += `
+        <div class="card-depoimento">
+                            <div>
+                                <img src="imagens/aspas.svg">
+                                <p class="body3">${depoimento.conteudo}</p>
+                            </div>
+                        <div class="dados-depoimento">
+                            <img src="${depoimento.imagem}">
+                            <div class="dados-depoimento-texto">
+                                <h6>${depoimento.autor}</h6>
+                                <p class="cargo">${depoimento.profissao}</p>
+                            </div>
+                        </div>
+        `
+    });
+}
